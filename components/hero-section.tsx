0c061f566ac37bb8a58bd04d2ad48454e3db3b1a
YuvanX <abhivigneshofficial@gmail.com>
2025-06-29 16:14:53 +0530

    feat: Add gsap lib

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,6 +1,5 @@
 export const HeroSection = () => {
     return <div className="min-h-screen items-center justify-center font-sans font-bold flex flex-col">
         <div className="text-9xl text-violet-700">nuvessi</div>
-        <div className="font-thin text-lg">Making yours into ours — a brand built together.</div>
     </div>
 }
