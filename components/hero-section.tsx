417ab3782807543abfe0ae4b47b51f6c7e47f849
YuvanX <abhivigneshofficial@gmail.com>
2025-06-29 16:41:03 +0530

    feat: Add stagger animation to herosection

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,5 +1,43 @@
+"use client";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
+gsap.registerPlugin(useGSAP);
+
 export const HeroSection = () => {
-    return <div className="min-h-screen items-center justify-center font-sans font-bold flex flex-col">
-        <div className="text-9xl text-violet-700">nuvessi</div>
+  const brandName = ["n", "u", "v", "e", "s", "s", "i"];
+  const halfLength = Math.floor(brandName.length / 2);
+
+  useGSAP(() => {
+    gsap.from(".start", {
+      y: 200,
+      duration: 0.6,
+      stagger: 0.1,
+    });
+
+    gsap.from(".end", {
+      y: 200,
+      duration: 0.6,
+      stagger: -0.1,
+    });
+  });
+
+  return (
+    <div className="min-h-screen items-center justify-center font-sans font-bold flex flex-col">
+      <div className="text-9xl text-violet-700 overflow-hidden">
+        {brandName.map((char, idx) =>
+          idx < halfLength ? (
+            <span key={idx} className="inline-block start">
+              {char}
+            </span>
+          ) : (
+            <span className="inline-block end" key={idx}>
+              {char}
+            </span>
+          )
+        )}
+      </div>
     </div>
-}
+  );
+};
