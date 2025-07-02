a302d90d8d449222d33725d6cd8404c58addb693
YuvanX <abhivigneshofficial@gmail.com>
2025-07-02 10:21:35 +0530

    animate brand-description part

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -7,9 +7,12 @@ export const HeroSection = () => {
   const [isOverlayCompleted, setIsOverlayCompleted] = useState(false);
 
   const brandName = ["s", "l", "a", "v", "a", "n", "."];
+  const brandDescription = "Making yours into ours - a brand built together.".split(' ');
+  
   const halfLength = Math.floor(brandName.length / 2);
 
   useEffect(() => {
+    
     const tl = gsap.timeline();
     tl.from(".start", {
       y: 200,
@@ -30,12 +33,27 @@ export const HeroSection = () => {
     tl.to(overlay.current, {
       y: "-100%",
     });
+
   }, []);
 
+  useEffect(() => {
+    if(!isOverlayCompleted) return;
+
+    gsap.from(".brand-description", {
+      y: 200,
+      duration: 0.3,
+      stagger: 0.1,
+    })
+  }, [isOverlayCompleted])
+
   return (
-    <div className="min-h-screen font-sans  flex flex-col relative">
+    <div className="min-h-screen font-sans relative">
       {isOverlayCompleted ? (
-        <div>Making yours into ours - a brand built together</div>
+        <div className="flex  justify-center items-center h-screen text-5xl font-semibold">
+          <div className="overflow-y-hidden py-3">
+              {brandDescription.map((b, idx) =>  <span key={idx} className="inline-block brand-description lowercase mr-2">{b}</span>)}
+          </div>
+        </div>
       ) : (
         <div className="text-9xl text-purple-500 overflow-hidden font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           {brandName.map((char, idx) =>
@@ -54,7 +72,7 @@ export const HeroSection = () => {
 
       <div
         ref={overlay}
-        className="h-screen w-screen bg-purple-500 absolute"
+        className="h-screen w-screen bg-purple-500 absolute top-0"
       ></div>
     </div>
   );
