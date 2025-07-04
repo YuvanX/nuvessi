7c96d20d6a89be3b09352710e14c2c23db110baa
YuvanX <abhivigneshofficial@gmail.com>
2025-07-04 20:22:32 +0530

    fix: projects page responsive

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ import { FaXTwitter } from "react-icons/fa6";
 export const Footer = () => {
   const router = useRouter();
   return (
-    <div className="absolute bottom-0 left-0 right-0 mx-10 flex justify-between items-end overflow-hidden">
+    <div className="fixed bottom-0 left-0 right-0 px-10 py-2 flex justify-between items-end overflow-hidden bg-background/30 border border-background/30 backdrop-blur-lg">
       <div className="text-5xl md:text-9xl text-[#E5E5E3] font-semibold tracking-tighter">
         slavan.
       </div>
--- components/projects.tsx
+++ components/projects.tsx
@@ -6,7 +6,6 @@ import { useEffect, useRef } from "react";
 import gsap from "gsap";
 
 export const Projects = () => {
-
   const PROJECTS = [
     {
       title: "unfold ui clone",
@@ -14,6 +13,7 @@ export const Projects = () => {
         "This is an unfold websites ui clone depicting the skills i have. built using tailwindcss, motion, nextjs.",
       year: "2025",
       image: "/images/unfold.png",
+      alt: "unfold ui clone",
     },
     {
       title: "nexus",
@@ -21,6 +21,7 @@ export const Projects = () => {
         "This is an full stack project, I have built it with miniaml design and its responsive. built using tailwindcss, motion, nextjs.",
       year: "2025",
       image: "/images/nexus.png",
+      alt: "nexus",
     },
     {
       title: "portfolio",
@@ -28,6 +29,7 @@ export const Projects = () => {
         "This is an dukaan ui clone depicting the skills i have. built using tailwindcss, react.",
       year: "2025",
       image: "/images/portfolio.png",
+      alt: "portfolio",
     },
     {
       title: "cool youtube clone",
@@ -35,6 +37,7 @@ export const Projects = () => {
         "This is an youtube clone remastering the design and in a minimal way. built using tailwindcss, motion, nextjs.",
       year: "2024",
       image: "/images/youtube.png",
+      alt: "youtube clone",
     },
     {
       title: "thoughts",
@@ -42,50 +45,64 @@ export const Projects = () => {
         "This a full stack blogging app, where i used different libraries for design.",
       year: "2024",
       image: "/images/thoughts.png",
+      alt: "thoughts",
     },
-    
   ];
 
   const titleRef = useRef(null);
-  
 
   useEffect(() => {
     const ctx = gsap.context(() => {
-      const tl = gsap.timeline()
+      const tl = gsap.timeline();
 
       tl.from(titleRef.current, {
-          y: -100,
-          opacity: 0,
-          duration: 0.3
+        y: -100,
+        opacity: 0,
+        duration: 0.3,
       }).from(".project-items", {
         y: 400,
         opacity: 0,
         duration: 0.5,
-        stagger: 0.1
-      })
-    })
+        stagger: 0.1,
+      });
+    });
 
     return () => ctx.revert();
-  }, [])
+  }, []);
 
   return (
     <div className="min-h-screen pt-20">
       <BackButton />
-      <div ref={titleRef} className="text-white text-5xl font-semibold text-center">
+      <div
+        ref={titleRef}
+        className="text-white text-5xl font-semibold text-left md:text-center mx-5"
+      >
         projects
       </div>
-      <div className="mt-20 flex flex-col items-center max-w-3xl gap-4 mx-auto w-full">
-        {PROJECTS.map((p, idx) => (<div key={idx} className="grid grid-cols-1 md:grid-cols-12 project-items">
-            <div className="col-span-2 px-2 py-2 font-medium">{p.year}</div>
-            <div className="col-span-8 flex flex-col px-4 py-2">
-                <div className="font-semibold text-xl lowercase">{p.title}</div>
-                <div className="text-muted-foreground lowercase">{p.description}</div>
+      <div className="my-20 flex flex-col items-center max-w-3xl gap-4 mx-auto w-full">
+        {PROJECTS.map((p, idx) => (
+          <div
+            key={idx}
+            className="grid grid-cols-1 md:grid-cols-12 project-items md:mx-10"
+          >
+            <div className="md:col-span-2 px-2 py-2 font-medium">{p.year}</div>
+            <div className="md:col-span-2 relative w-[100px] h-[100px] mx-2 md:order-last">
+              <Image
+                src={p.image}
+                alt={p.alt}
+                fill
+                className="rounded"
+                style={{ objectFit: "cover", objectPosition: "center" }}
+              />
             </div>
-            <div className="col-span-2 px-2 py-2 relative">
-              <Image src={p.image} alt="hi" fill className="rounded" style={{ objectFit: 'cover', objectPosition: 'center'}} />
+            <div className="md:col-span-8 flex flex-col px-2 py-2">
+              <div className="font-semibold text-xl lowercase">{p.title}</div>
+              <div className="text-muted-foreground lowercase">
+                {p.description}
+              </div>
             </div>
-           
-        </div>))}
+          </div>
+        ))}
       </div>
     </div>
   );
