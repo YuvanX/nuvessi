e5c1281f3513eab35d3281ab8c230f3867d82021
YuvanX <abhivigneshofficial@gmail.com>
2025-07-04 21:38:47 +0530

    fix: root page name

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,4 @@
 import { Footer } from "@/components/footer";
-import { Provider } from "@/lib/provider";
 import React from "react";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { HeroSection } from "@/components/hero-section";
 import { TopBar } from "@/components/top-bar";
 
-export default function () {
+export default function HomePage() {
   return (
     <div className="bg-[#101010] overflow-hidden">
       <TopBar />
