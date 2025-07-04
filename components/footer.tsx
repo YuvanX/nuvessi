bb27638e2c42b0c36b2bd9c82968eed136befc4c
YuvanX <abhivigneshofficial@gmail.com>
2025-07-04 18:38:37 +0530

    fix: make footer responsive

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,12 +6,12 @@ export const Footer = () => {
   const router = useRouter();
   return (
     <div className="absolute bottom-0 left-0 right-0 mx-10 flex justify-between items-end overflow-hidden">
-      <div className="text-9xl text-[#E5E5E3] font-semibold tracking-tighter">
+      <div className="text-5xl md:text-9xl text-[#E5E5E3] font-semibold tracking-tighter">
         slavan.
       </div>
-      <div className="mb-5 flex gap-10 text-[#E5E5E3]">
-       <FaInstagram onClick={() => router.push("https://www.instagram.com/slavanhq/")} size={20} />
-       <FaXTwitter size={20} />
+      <div className="mb-2 md:mb-5 flex gap-8 md:gap-10 text-[#E5E5E3]">
+       <FaInstagram onClick={() => router.push("https://www.instagram.com/slavanhq/")}  className="text-xs md:text-xl"/>
+       <FaXTwitter className="text-xs md:text-xl" />
       </div>
     </div>
   );
